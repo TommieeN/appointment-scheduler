0e79ea7ff05f0e5d1346dd48f26949ae3de639e3
TommyNguyen <timsnguyen@hotmail.com>
2024-08-05 12:08:42 -0700

    adjust and create tables for appts and customers

--- server/migrations/20240805190017_create_customers_table.js
+++ server/migrations/20240805190017_create_customers_table.js
@@ -2,20 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable("customer", (table) => {
-    table.increments('id').primary();
-    table.string("first_name").notNullable()
-    table.string("last_name").notNullable()
-    table.string("phone_number").notNullable().unique()
+    table.increments("customer_id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("phone_number").notNullable().unique();
     table.string("email").notNullable().unique();
-  })
+    table.timestamps(true, true)
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable("customer");
 };
---
+++ server/migrations/20240805190040_create_appointments_table.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("appointment", (table) => {
+    table.increments("appointment_id").primary();
+    table
+      .integer("customer_id")
+      .unsigned()
+      .notNullable()
+      .references("customer_id")
+      .inTable("customer")
+      .onDelete("CASCADE");
+    // table.integer("employee_id").unsigned().notNullable().references("employee_id").inTable("employee").onDelete("CASCADE");
+    // table.integer("service_id").unsigned().notNullable().references("service_id").inTable("service").onDelete("CASCADE");
+    table.timestamp("appointment_date").notNullable();
+    table.string("status").notNullable();
+    table.text("notes");
+    table.timestamps(true,true)
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("appointment");
+};
