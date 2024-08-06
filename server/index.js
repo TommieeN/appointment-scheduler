c7444c4fe5acd3b81b5ebace6ba312f2c3b75e2d
TommyNguyen <timsnguyen@hotmail.com>
2024-08-05 17:43:07 -0700

    route setup

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const db = require("./db");
+const cors = require("cors");
+const app = express();
+
+const authRoutes = require("./routes/authRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
+
+const PORT = process.env.port || 8080;
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.port || 8080;
+app.use(cors());
 
-app.use(async (req, res, next) => {
-  try {
-    await db.raw("SELECT 1");
-    console.log("Database connected succesfully");
-    next();
-  } catch (error) {
-    console.error("Database connection failed:", error);
-    res.status(500).send("Database connection failed");
-  }
+app.use("/api/auth", authRoutes);
+app.use("/api/customers", customerRoutes);
+app.use("/api/appointments", appointmentRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
-
-app.get('/', (req, res) => {
-    res.send("Hello, world!")
-})
 
 app.listen(PORT, () => {
   console.log(`App is running on port:${PORT}`);
--- server/migrations/20240805203251_create_service_table.js
+++ server/migrations/20240805203251_create_service_table.js
@@ -5,8 +5,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable("service", (table) => {
     table.increments("service_id").primary()
-    table.string("service_name")
-    table.string("description")
+    table.string("service_name", 50)
+    table.string("description", 200)
     table.double("price")
     table.integer("duration_minutes")
   })
